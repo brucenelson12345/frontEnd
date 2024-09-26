c389ecefc1461e3f513394e008634eab3498b111
kainewhite <kainewhite@gmail.com>
2024-09-26 09:34:03 -0600

    training

--- quantizer/quantizer.py
+++
@@ -1,6 +0,0 @@
-import aimet_zoo_torch
-from efficientnet_lite_pytorch import EfficientNet
-from efficientnet_lite0_pytorch_model import EfficientnetLite0ModelFile
-
-weights_path = EfficientnetLite0ModelFile.get_model_file_path()
-model = EfficientNet.from_pretrained('efficientnet-lite0', weights_path = weights_path)
--- yolov8/onnx/yolov8_export_onnx.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8n.pt')
-model.export(format = 'onnx') # exports the model in '.onnx' format
